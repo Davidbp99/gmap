a2c6942dc4a036b9dbce6839cbafac93eacf1a9b
Davidbp99 <40523727+Davidbp99@users.noreply.github.com>
2021-05-15 19:59:55 +0200

    Initial commit

---
+++ lua/autorun/map_script.lua
@@ -0,0 +1,51 @@
+print("mapscript loaded")
+
+
+
+local fullaccess = {
+["superadmin"] = true,
+
+
+}
+
+local donthaveaccess = "You Can't Access This"
+
+local JobDoors = {
+ -- [mapid] = "job name", (job name does not team TEAM_)
+}
+
+local gamemasters = {
+
+
+
+}
+
+
+
+local RankDoors = {
+[3834] = "superadmin", -- Kill floor
+[3837] = "gamemasters",
+[3903] = "gamemasters",
+[3893] = "gamemasters",
+}
+
+
+
+hook.Add("PlayerUse", "DoorJobs", function(pl, ent)
+--  // door permission
+
+
+
+  if (JobDoors[ent:MapCreationID()]) or (RankDoors[ent:MapCreationID()]) then 
+   -- if (fullaccess[pl:GetUserGroup()] == true) then return end -- To add your own rank so management and superadmins don't need to be added to 200 buttons
+  	  if (pl:GetUserGroup() == RankDoors[ent:MapCreationID()]) then -- here it gets the rank and checks if it should have perms
+  	  	 return 
+  	  	 else
+pl:SendMessage(donthaveaccess)
+  	  	 end
+    if (pl:getDarkRPVar("job") ~= JobDoors[ent:MapCreationID()]) then -- here it gets the jobs and checks if it should have perms
+    	
+      return false
+    end
+end
+end)
