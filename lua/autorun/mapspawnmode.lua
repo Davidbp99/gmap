f718e30fb847c66a9f7cfe78addbbec402e30604
Davidbp99 <40523727+Davidbp99@users.noreply.github.com>
2021-07-13 12:25:16 +0200

    Added gmap:print() to show its done and made it run only code on my map
    Left some notes to help out understand the code

--- lua/autorun/mapspawnmode.lua
+++ lua/autorun/mapspawnmode.lua
@@ -1,13 +1,9 @@
 gMap:Print("RP_Downtown_tits_david Addon Loaded")
-
-
-
-
+--// Set the spawn mode. false = it got the teleport + stairs up to main square.  true= just the teleports
 local spawnmodeenabled = false
-local spawnmodebutton = "disablespawn" -- name for the button that activates the wall brushes in main square and in spawn
-
 --// Disable teleports
 -- mainsquaretpbutton , beachtpbutton , parktpbutton, warehousetpbutton, pvptpbutton, estatestpbutton
+-- As this only runs once while you start up the server/singleplayer you will need to config it how you want it from the start.
 local disablemainsquaretp = false
 local disablebeachtp = false
 local disableparktp = false
@@ -16,52 +12,52 @@ local disablepvptp = false
 local disableestatestp = false
 
 --//
-
-
-hook.Add("Initialize", "Automatedmap", function ()
-timer.Simple(5, function() 
-if spawnmodeenabled == true then
-    for k,v in pairs(ents.FindByName(spawnmodebutton)) do
-        v:Fire( "use" )
-        end
+hook.Add("Initialize", "Automatedmap", function()
+    if (game.GetMap() == "rp_downtown_tits_david_*") then
+        timer.Simple(5, function()
+            if spawnmodeenabled == true then
+                for k, v in pairs(ents.FindByName("disablespawn")) do
+                    v:Fire("use")
+                end
             end
 
-if disablemainsquaretp == true then
-    for k,v in pairs(ents.FindByName("mainsquaretpbutton")) do
-        v:Fire( "use" )
-        end
+            if disablemainsquaretp == true then
+                for k, v in pairs(ents.FindByName("mainsquaretpbutton")) do
+                    v:Fire("use")
+                end
             end
 
-if disablebeachtp == true then
-    for k,v in pairs(ents.FindByName("beachtpbutton")) do
-        v:Fire( "use" )
-        end
+            if disablebeachtp == true then
+                for k, v in pairs(ents.FindByName("beachtpbutton")) do
+                    v:Fire("use")
+                end
             end
 
-
-if disableparktp == true then
-    for k,v in pairs(ents.FindByName("parktpbutton")) do
-        v:Fire( "use" )
-        end
+            if disableparktp == true then
+                for k, v in pairs(ents.FindByName("parktpbutton")) do
+                    v:Fire("use")
+                end
             end
 
-if disablewarehousetp == true then
-    for k,v in pairs(ents.FindByName("warehousetpbutton")) do
-        v:Fire( "use" )
-        end
+            if disablewarehousetp == true then
+                for k, v in pairs(ents.FindByName("warehousetpbutton")) do
+                    v:Fire("use")
+                end
             end
 
-if disablepvptp == true then
-    for k,v in pairs(ents.FindByName("pvptpbutton")) do
-        v:Fire( "use" )
-        end
+            if disablepvptp == true then
+                for k, v in pairs(ents.FindByName("pvptpbutton")) do
+                    v:Fire("use")
+                end
             end
 
-if disableestatestp == true then
-    for k,v in pairs(ents.FindByName("estatestpbutton")) do
-        v:Fire( "use" )
-        end
+            if disableestatestp == true then
+                for k, v in pairs(ents.FindByName("estatestpbutton")) do
+                    v:Fire("use")
+                end
             end
 
-end)
+            gMap:Print("Map Settings Have Deployed")
+        end)
+    end
 end)
