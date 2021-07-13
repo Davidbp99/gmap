6c788a8df255b9655eb76969e877e9ea18eaf411
Davidbp99 <40523727+Davidbp99@users.noreply.github.com>
2021-07-13 04:11:49 +0200

    [Beta] Automated new map spawn mode + teleport

--- lua/autorun/mapspawnmode.lua
+++ lua/autorun/mapspawnmode.lua
@@ -3,13 +3,63 @@ gMap:Print("RP_Downtown_tits_david Addon Loaded")
 
 
 
-local spawnmodebutton = 4086 -- ID for the button that activates the wall brushes in main square and in spawn
+local spawnmodeenabled = true
+local spawnmodebutton = "disablespawn" -- name for the button that activates the wall brushes in main square and in spawn
 
 --// Disable teleports
+-- mainsquaretpbutton , beachtpbutton , parktpbutton, warehousetpbutton, pvptpbutton, estatestpbutton
+local disablemainsquaretp = true
+local disablebeachtp = false
+local disableparktp = false
+local disablewarehousetp = false
+local disablepvptp = true
+local disableestatestp = true
 
 --//
-function SpawnMode()
-    if IsValid( spawnmodebutton ) then
-    spawnmodebutton:Fire("use")
-end
-end
+
+timer.Simple(5, function() 
+if spawnmodeenabled == true then
+    for k,v in pairs(ents.FindByName(spawnmodebutton)) do
+        v:Fire( "use" )
+        end
+            end
+
+if disablemainsquaretp == true then
+    for k,v in pairs(ents.FindByName("mainsquaretpbutton")) do
+        v:Fire( "use" )
+        end
+            end
+
+if disablebeachtp == true then
+    for k,v in pairs(ents.FindByName("beachtpbutton")) do
+        v:Fire( "use" )
+        end
+            end
+
+
+if disableparktp == true then
+    for k,v in pairs(ents.FindByName("parktpbutton")) do
+        v:Fire( "use" )
+        end
+            end
+
+if disablewarehousetp == true then
+    for k,v in pairs(ents.FindByName("warehousetpbutton")) do
+        v:Fire( "use" )
+        end
+            end
+
+if disablepvptp == true then
+    for k,v in pairs(ents.FindByName("pvptpbutton")) do
+        v:Fire( "use" )
+        end
+            end
+
+if disableestatestp == true then
+    for k,v in pairs(ents.FindByName("estatestpbutton")) do
+        v:Fire( "use" )
+        end
+            end
+
+end)
+
